91dce9a2d95ad69c5652f6c7876c3bc79987d7b0
Sujith Bonthala <78267490+SujithBonthala@users.noreply.github.com>
2023-02-17 19:28:04 +0530

    Create Lab-2a.cpp

---
+++ Lab-2a.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Creating a pipeline to build, test and deploy applications\n";
+  cout << "This facilitates automation of the development process";
+  return 0;
+}
